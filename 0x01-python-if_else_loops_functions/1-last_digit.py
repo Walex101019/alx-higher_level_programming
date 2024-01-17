3704045ccb45d2e6111d7a0534373f1389116d30
Walex101019 <ibietanwasiu94@gmail.com>
2024-01-17 10:05:54 -0800

    generates random number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,4 +8,4 @@ if last_digit > 5:
 elif last_digit == 0:
     print(message + " and is 0")
 elif last_digit < 6 and last_digit != 0:
-    print(message + "  and is less than 6 and not 0")
+    print(message + " and is less than 6 and not 0")
