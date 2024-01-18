45ffdbbefd5c86f33d4aaa79514017fc66566133
Walex101019 <ibietanwasiu94@gmail.com>
2024-01-17 18:27:14 -0800

    uppercase and lowercase functions

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
-    """Function converts a lowercase letter to uppercase."""
-    if 'a' <= str <= 'z':
-        return chr(ord(str) - ord('a') + ord('A'))
-    else:
-        return str
+    result = ""
+    for i in str:
+        if ord('a') <= ord(i) <= ord('z'):
+            result += chr(ord(i) - 32)
+        else:
+            result += i
+    print("{}".format(result))
