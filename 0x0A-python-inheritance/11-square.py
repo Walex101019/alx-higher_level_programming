98e6c0a703cbc502e45a582cbd1b93d44105419a
Walex101019 <ibietanwasiu94@gmail.com>
2024-02-20 13:39:03 -0800

    fixed square 11

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,9 +1,15 @@
- a Rectangle subclass Square."""
+""" a Rectangle subclass Square."""
 Rectangle = __import__('9-rectangle').Rectangle
 
-
 class Square(Rectangle):
-    """Represent a square."""
+    """Represents a square.
+
+    This class is a subclass of Rectangle and represents a square. It inherits
+    attributes and methods from the Rectangle class.
+
+    Attributes:
+        size (int): The size of the square's sides.
+    """
 
     def __init__(self, size):
         """Initialize a new square.
